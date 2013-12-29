87cc608661b1d661dccf7728a23b37ab6d960d48
nadasdi.mate <nadasdi.mate@ustream.tv>
2013-12-29 23:04:40 +0100

    workshops section

--- 2014/src/documents/themes/craftconf/js/main.js
+++ 2014/src/documents/themes/craftconf/js/main.js
@@ -1,155 +1,163 @@
 ;(function ($, window, document) {
+	//Shim for requestAnimationFrame
+	window.requestAnimFrame = (function(){
+		return  window.requestAnimationFrame       ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame    ||
+			function( callback ){
+				window.setTimeout(callback, 1000 / 60);
+			};
+	})();
+
+	//Application object
 	var app = {
 		body: $(document.body),
+		htmlBody: $('html, body'),
 
-		animateScroll : function () {
-			$("#nav").on('click', 'a', function (event) {
+		gears: $(".gear"),
+		gearSpeeds: {
+			gear_1: -0.9,
+			gear_2: 1.2,
+			gear_3: -.8,
+			gear_4: 1.1,
+			gear_5: -1,
+			gear_6: 2
+		},
 
-				var $this = $(this),
-					$htmlBody = $('html, body'),
-					linkTarget = $this.attr('href'),
-					linkSplits = linkTarget.split('#'),
-					offSetTop;
+		/**
+		 * Navigation item click handler
+		 * @method onNavClick
+		 * @param {Event} e
+		 */
+		onNavClick: function (e) {
+			var target = $(e.target),
+				linkTarget = target.attr('href'),
+				linkSplits = linkTarget.split('#'),
+				offSetTop;
 
-				// If no hashtag found, stop here!
-				if (linkSplits.length < 2) {
-					return true;
-				}
+			// If no hashtag found, stop here!
+			if (linkSplits.length < 2) {
+				return;
+			}
 
-				linkTarget = '#' + linkSplits[1];
+			linkTarget = '#' + linkSplits[1];
 
-				event.preventDefault();
+			e.preventDefault();
 
-				// Get distance of top
-				offSetTop = $(linkTarget).offset().top;
+			// Get distance of top
+			offSetTop = $(linkTarget).offset().top;
 
-				// Animate the scroll
-				$htmlBody.stop().animate({scrollTop : offSetTop}, function () {
-					location.hash = linkTarget;
-				});
+			// Animate the scroll
+			this.htmlBody.stop().animate({scrollTop : offSetTop}, function () {
+				location.hash = linkTarget;
 			});
 		},
 
+		/**
+		 * Gear animation setup, rotate on window scroll event
+		 * @method setupGearAnimation
+		 */
 		setupGearAnimation: function () {
-			var gears = $(".gear"),
-				config = {
-					gear_1: -0.9,
-					gear_2: 1.2,
-					gear_3: -.8,
-					gear_4: 1.1,
-					gear_5: -1,
-					gear_6: 2
-				};
-
-			window.requestAnimFrame = (function(){
-				return  window.requestAnimationFrame       ||
-					window.webkitRequestAnimationFrame ||
-					window.mozRequestAnimationFrame    ||
-					function( callback ){
-						window.setTimeout(callback, 1000 / 60);
-					};
-			})();
-
-			$(window).on('scroll', function (e) {
+			$(window).on('scroll', $.proxy(function () {
 				var deg = - window.scrollY * 360 / document.body.offsetHeight;
 
-				window.requestAnimationFrame(function () {
-					gears.each(function (i, e) {
+				window.requestAnimationFrame($.proxy(function () {
+					this.gears.each($.proxy(function (i, e) {
 						e = $(e);
 
-						deg *= config['gear_' + e.data('gear')] || 0.5;
+						deg *= this.gearSpeeds['gear_' + e.data('gear')] || 0.5;
 
 						e.css('transform', 'translateZ(0) rotate('+ deg + 'deg)');
-					});
-				});
-			});
+					}, this));
+				}, this));
+			}, this));
 		},
 
-		setupDropdown: function () {
-			var dropdown = $('#dropdown');
+		/**
+		 * Header dropDown functionality on mobile/tablet devices
+		 * @method setupDropDown
+		 */
+		setupDropDown: function () {
+			var dropDown = $('#dropdown');
 
 			this.body.on('click', function (e) {
 				var target = $(e.target);
 
 				if (!target.closest('#dropdown').length && !target.closest('#menu').length) {
-					dropdown.removeClass('opened');
+					dropDown.removeClass('opened');
 				}
 			});
 
 			$('.nav-item a').on('click', function () {
-				dropdown.removeClass('opened');
+				dropDown.removeClass('opened');
 			});
 
 			$('#menu').on('click', function () {
-				dropdown.toggleClass('opened');
+				dropDown.toggleClass('opened');
 			});
 		},
 
+		/**
+		 * Speaker navigation setup
+		 * @method setupSpeakerList
+		 */
 		setupSpeakerList: function () {
-			var speakers = $('.speakers-list');
-
 			//Speaker item click
-			speakers.on('click', 'li', $.proxy(function (e) {
-				var target = $(e.target);
+			$('.speakers-list').on('click', 'li', $.proxy(this.onSpeakerClick, this));
 
-				if (target.hasClass('twitter-link')) return;
-
-				target = target.closest('li');
-
-				this.openSpeaker(target.data('type'),  target.attr('id'));
-			}, this));
-
-			//Featured link click
-			$('.featured a').on('click', $.proxy(function (e) {
-				var href = $(e.target).attr('href'),
-					id = href.replace('#speakers/', '');
-
-				e.preventDefault();
-
-				$('.speakers-list li')
-					.addClass('notransition')
-					.removeClass('opened')
-					.find('.more').height(0)
-					.removeClass('notransition');
-
-				this.openSpeaker('speakers', id, true);
-			}, this));
+			//Featured speaker click
+			$('.featured a').on('click', $.proxy(this.onFeaturedSpeakerClick, this));
 		},
 
-		improveScrolling: function () {
-			var timer,
-				speakers = $('.speakers-list');
+		/**
+		 * Speaker item click handler
+		 * @method onSpeakerClick
+		 * @param {Event} e
+		 */
+		onSpeakerClick: function (e) {
+			var target = $(e.target);
 
-			$(window).on('scroll', $.proxy(function () {
-				clearTimeout(timer);
-				if(!speakers.hasClass('disable-hover')) {
-					speakers.addClass('disable-hover')
-				}
+			if (target.hasClass('twitter-link')) return;
 
-				timer = setTimeout($.proxy(function(){
-					speakers.removeClass('disable-hover')
-				} , this), 200);
-			}, this));
+			target = target.closest('li');
+
+			this.openSpeaker(target.data('type'),  target.attr('id'));
 		},
 
-		handleHash: function (navigate) {
-			var hash = window.location.hash,
-				matches = hash.match(/#(speakers|workshops)\/(.*)/);
+		/**
+		 * Featured speaker click handler in about section
+		 * @method onFeaturedSpeakerClick
+		 * @param {Event} e
+		 */
+		onFeaturedSpeakerClick: function (e) {
+			var href = $(e.target).attr('href'),
+				id = href.replace('#speakers/', '');
 
-			if (matches && matches[1]) {
-				setTimeout($.proxy(function () {
-					this.openSpeaker(matches[1], matches[2], navigate);
-				}, this), 10);
-			}
+			e.preventDefault();
+
+			$('.speakers-list li')
+				.addClass('notransition')
+				.removeClass('opened')
+				.find('.more').height(0)
+				.end()
+				.removeClass('notransition');
+
+			this.openSpeaker('speakers', id, true);
 		},
 
+		/**
+		 * Open speaker by type and id parameter
+		 * @param {speakers|workshops} type
+		 * @param {String} id
+		 * @param {Boolean=} navigate
+		 */
 		openSpeaker: function (type, id, navigate) {
 			var speaker = $('#' + id + '[data-type="' + type + '"]'),
 				more = speaker.find('.more'),
 				speechInfo = speaker.find('.speech-info');
 
 			if (navigate) {
-				this.body.animate({
+				this.htmlBody.animate({
 					scrollTop: speaker.offset().top - 80
 				});
 			}
@@ -172,22 +180,74 @@
 			}
 		},
 
+		/**
+		 * Improves scrolling performance by disabling pointer events during scroll
+		 * (pointer-events: none;)
+		 * @method improveScrolling
+		 */
+		improveScrolling: function () {
+			var timer,
+				speakers = $('.speakers-list');
+
+			$(window).on('scroll', $.proxy(function () {
+				clearTimeout(timer);
+				if(!speakers.hasClass('disable-hover')) {
+					speakers.addClass('disable-hover')
+				}
+
+				timer = setTimeout($.proxy(function(){
+					speakers.removeClass('disable-hover')
+				} , this), 200);
+			}, this));
+		},
+
+		/**
+		 * Initial hash handler
+		 * @method handleInitialHash
+		 */
+		handleInitialHash: function () {
+			var hash = window.location.hash,
+				matches = hash.match(/#(speakers|workshops)\/(.*)/);
+
+			if (matches && matches[1]) {
+				setTimeout($.proxy(function () {
+					this.openSpeaker(matches[1], matches[2], true);
+				}, this), 10);
+			}
+		},
+
+		/**
+		 * Touch device check
+		 * @method isTouchDevice
+		 * @returns {boolean|*}
+		 */
+		isTouchDevice: function () {
+			return (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch);
+		},
+
+		/**
+		 * @constructor
+		 */
 		init : function () {
 			//Skip scroll animation on touch devices
-			if(!(('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch)) {
+			if(!this.isTouchDevice()) {
 				this.setupGearAnimation();
 				this.improveScrolling();
 			} else {
 				this.body.addClass('touch');
 			}
 
-			this.setupDropdown();
+			//Navigation
+			$("#nav").on('click', 'a', $.proxy(this.onNavClick, this));
+			this.setupDropDown();
+
+			//Speakers
 			this.setupSpeakerList();
-			this.animateScroll();
-			this.handleHash(true);
+			this.handleInitialHash();
 		}
 	};
 
+	//Let's rock!
 	$(function () {
 		app.init();
 	});
