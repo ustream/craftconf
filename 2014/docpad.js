dff1c247557bb294f1761b135f44d7bdfab53367
felho <felho@ustream.tv>
2014-04-21 00:11:15 -0700

    Serf -> Consul

--- 2014/docpad.js
+++ 2014/docpad.js
@@ -804,7 +804,7 @@ module.exports = {
 				anchor: "MitchellHashimoto",
 				name: "Mitchell Hashimoto",
 				photo: "themes/craftconf/img/speakers/MitchellHashimoto.png",
-				bio: "<p>Mitchell is best known as the creator of Vagrant, Packer, and Serf. Mitchell is the founder of HashiCorp, a company that builds powerful and elegant DevOps tools. He is also an O’Reilly author and professional speaker. He is one of the top GitHub users by followers, activity, and contributions. “Automation obsessed,” Mitchell solves problems with as much computer automation as possible.</p>",
+				bio: "<p>Mitchell is best known as the creator of Vagrant, Packer, and Consul. Mitchell is the founder of HashiCorp, a company that builds powerful and elegant DevOps tools. He is also an O’Reilly author and professional speaker. He is one of the top GitHub users by followers, activity, and contributions. “Automation obsessed,” Mitchell solves problems with as much computer automation as possible.</p>",
 				company: "HashiCorp",
 				link: {
 					href: "http://twitter.com/mitchellh",
@@ -812,7 +812,7 @@ module.exports = {
 				},
 				presentation: {
 					title: "Vagrant, Packer, Consul: Maximum Potency DevOps",
-					description: "In 2013, we saw the introduction of many new DevOps tools and existing tools such as Vagrant saw some huge improvements and features added. With all this change, its hard to keep up with not only what has changed, but how to use these changes and what benefits they have.<br><br>This talk begins by briefly introducing a handful of my own tools: Vagrant, Packer, and Serf. I talk about what their purpose is, some of their latest features, and who is using these tools. I then continue to show how these tools can be used along with other software such as Docker to create some truly amazing development and production stacks. For production stacks, I discuss the benefits of using some of these tools over existing options, and for development stacks, I show how Vagrant improves process and workflow to help streamline the entire process from development through to production.",
+					description: "In 2013, we saw the introduction of many new DevOps tools and existing tools such as Vagrant saw some huge improvements and features added. With all this change, its hard to keep up with not only what has changed, but how to use these changes and what benefits they have.<br><br>This talk begins by briefly introducing a handful of my own tools: Vagrant, Packer, and Consul. I talk about what their purpose is, some of their latest features, and who is using these tools. I then continue to show how these tools can be used along with other software such as Docker to create some truly amazing development and production stacks. For production stacks, I discuss the benefits of using some of these tools over existing options, and for development stacks, I show how Vagrant improves process and workflow to help streamline the entire process from development through to production.",
 				}
 			},
 			{
